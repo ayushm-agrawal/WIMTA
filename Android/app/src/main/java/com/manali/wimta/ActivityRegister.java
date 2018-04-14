703c2c55bf9f9b54584453e359aa2262d1ba2781
Atharva Tendle <atharva.tendle@huskers.unl.edu>
2018-04-14 16:47:16 -0500

    Added Email and Password Authentication

--- Android/app/src/main/java/com/manali/wimta/ActivityRegister.java
+++ Android/app/src/main/java/com/manali/wimta/ActivityRegister.java
@@ -4,24 +4,16 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 
 
 public class ActivityRegister extends AppCompatActivity implements View.OnClickListener{
@@ -84,8 +76,7 @@ public class ActivityRegister extends AppCompatActivity implements View.OnClickL
 
                 if (task.isSuccessful()) {
                       Toast.makeText(getApplicationContext(), "Registering!", Toast.LENGTH_SHORT).show();
-                      
-//                    startActivity(new Intent(SignUpActivity.this, ProfileActivity.class));
+//                      startActivity(new Intent(ActivityRegister.this, ActivityCreateBasicProfile.class));
                 } else {
 
                     if (task.getException() instanceof FirebaseAuthUserCollisionException) {
