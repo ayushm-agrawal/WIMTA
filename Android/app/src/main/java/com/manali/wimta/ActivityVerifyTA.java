21361ab3cb38d05072f900ad9d0d5e199f1add16
Atharva Tendle <atharva.tendle@huskers.unl.edu>
2018-04-15 05:43:31 -0500

    UI Changes

--- Android/app/src/main/java/com/manali/wimta/ActivityVerifyTA.java
+++ Android/app/src/main/java/com/manali/wimta/ActivityVerifyTA.java
@@ -1,7 +1,32 @@
 package com.manali.wimta;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseError;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ActivityVerifyTA extends AppCompatActivity {
 
@@ -9,5 +34,9 @@ public class ActivityVerifyTA extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_ta);
+
+
     }
+
+
 }
